37f9a343577ae07a91f5e6d37551c8f8327281c1
Watch your step! <cathy033077@gmail.com>
2023-03-31 22:46:16 +0900

    feat : README

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hear_sitter/src/core/app_router.dart';
 import 'package:hear_sitter/src/core/app_theme.dart';
 import 'package:hear_sitter/src/core/utils/sharedprefs_util.dart';
-
-import 'package:hear_sitter/src/providers/test.dart';
 import 'package:sound_stream/sound_stream.dart';
+
 late bool isOnboard;
 RecorderStream recorderStream = RecorderStream();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferencesUtil.initialize();
